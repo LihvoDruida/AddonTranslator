9c9ad7287f7ebbce58b53abf05efec3d08eb40c5
Galitsky Dmytro <melles.android@gmail.com>
2023-07-21 23:20:25 +0300

    Instead of individual checks and function calls for each translator, a for loop is now used in conjunction with an array for convenient management of translators.
    Changes made:
    1. Added the "translators" table to store translator data. Each translator is represented as an object with "enabled" (boolean value) and "func" (translation function) fields.
    2. Eliminated code duplication in the function calls of translators by moving the checks into the for loop.
    3. Added comments to the code to explain the new data structures and logic.

--- AddonTranslator.lua
+++ AddonTranslator.lua
@@ -15,10 +15,40 @@ local function createInterfaceOptions()
   configDialogLib:AddToBlizOptions(namespace, "Причетні", "AddonTranslator", "Contributors")
 end
 
-local function initializeAddon()
-  if (initialized) then return end
+local eventHandler = ns.EventHandler:new()
 
-  StaticPopupDialogs["WOW_UKRAINIZAER_RESET_SETTINGS"] = {
+local function OnPlayerLogin()
+  settingsProvider:Load()
+  createInterfaceOptions()
+
+  local translateAngryKeystones, translateOmniCC, translateRCLoot, translateDetails, translateRio, translateRareScanner, translateScrap, translateWeakAuras, translateAdiBags =
+      settingsProvider.GetTranslatorsState()
+
+  local translators = {
+    { enabled = translateAngryKeystones, func = LoadAngryKeystonesUkrainizer },
+    { enabled = translateOmniCC,         func = LoadOmniCCUkrainizer },
+    { enabled = translateRCLoot,         func = LoadRCLootUkrainizer },
+    { enabled = translateDetails,        func = LoadDetailsUkrainizer },
+    { enabled = translateRio,            func = LoadRioUkrainizer },
+    { enabled = translateRareScanner,    func = LoadRareScannerUkrainizer },
+    { enabled = translateScrap,          func = LoadScrapUkrainizer },
+    { enabled = translateWeakAuras,      func = LoadWeakAurasUkrainizer },
+    { enabled = translateAdiBags,        func = LoadAdiBagsUkrainizer },
+  }
+
+  for _, translator in ipairs(translators) do
+    if translator.enabled then
+      translator.func()
+    end
+  end
+end
+
+local function initializeAddon()
+  if initialized then
+    return
+  end
+
+  StaticPopupDialogs["ADDON_TRANSLATOR_RESET_SETTINGS"] = {
     text = "Ви впевнені, що хочете скинути всі налаштування до стандартних значень?",
     button1 = "Продовжити",
     button2 = "Скасувати",
@@ -31,43 +61,8 @@ local function initializeAddon()
   }
 end
 
-local eventHandler = ns.EventHandler:new()
-
 local function OnAddOnLoaded(_, name)
-  local function OnPlayerLogin()
-    settingsProvider:Load()
-    createInterfaceOptions();
-
-    local translateAngryKeystones, translateOmniCC, translateRCLoot, translateDetails, translateRio, translateRareScanner, translateScrap, translateWeakAuras =
-        settingsProvider.GetTranslatorsState()
-
-    if (translateAngryKeystones) then
-      LoadAngryKeystonesUkrainizer()
-    end
-    if (translateOmniCC) then
-      LoadOmniCCUkrainizer()
-    end
-    if (translateRCLoot) then
-      LoadRCLootUkrainizer()
-    end
-    if (translateDetails) then
-      LoadDetailsUkrainizer()
-    end
-    if (translateRio) then
-      LoadRioUkrainizer()
-    end
-	if (translateRareScanner) then
-      LoadRareScannerUkrainizer()
-    end
-	if (translateScrap) then
-      LoadScrapUkrainizer()
-    end
-	if (translateWeakAuras) then
-      LoadWeakAurasUkrainizer()
-    end
-  end
-
-  if (name == addonName) then
+  if name == addonName then
     initializeAddon()
     if not IsLoggedIn() then
       eventHandler:Register(OnPlayerLogin, "PLAYER_LOGIN")
